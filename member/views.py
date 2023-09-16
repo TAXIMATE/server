88f5a40581e51c32048fd4a776d117616d0b0e29
ShinSub98 <wjdtjq26@naver.com>
2023-09-16 15:57:25 +0900

    카카오 로그인 메소드 작성 중

--- member/views.py
+++ member/views.py
@@ -12,14 +12,27 @@ from django.db.models import Q
 import requests
 
 # Create your views here.
-# class KakaoCallBackView(View):
-#     def get(self, request):
-#         data = {
-#             "grant_type" : "authorization_code",
-#             "client_id" : "",
-#             "redirect_uri" : "",
-#             "code" : request.GET["code"]
-#         }
+class KakaoCallBackView(View):
+    def get(self, request):
+        data = {
+            "grant_type" : "authorization_code",
+            "client_id" : "",
+            "redirect_uri" : "",
+            "code" : request.GET["code"]
+        }
 
-#         kakao_token_api = "https://kauth.kakao.com/oauth/token"
-#         access_token = requests.post
+        kakao_token_api = "https://kauth.kakao.com/oauth/token"
+        access_token = requests.post(kakao_token_api, data = data).json()["access_token"]
+
+        kakao_user_api = "http://kapi.kakao.com/v2/user/me"
+        header = {"Authorization":f"Bearer ${access_token}"}
+        user_information = requests.get(kakao_user_api, headers = header).json()
+
+        kakao_id = user_information["id"]
+        kakao_email = user_information["kakao_account"]["email"]
+        profile_image = user_information["properties"]["profile_image"]
+        nickname = user_information["properties"]["nickname"]
+
+        
+
+
