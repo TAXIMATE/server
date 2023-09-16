071fe75adca8891ee6aeba78696b9110047d77a9
ShinSub98 <wjdtjq26@naver.com>
2023-09-16 16:32:14 +0900

    Team 모델 작성

--- member/models.py
+++ member/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from team.models import *
+from taximate.team.models import Team
 
 # Create your models here.
 class User(models.Model):
-    kakao_id = models.CharField(max_length=100)
+    kakao_id = models.BigIntegerField(unique=True)
     kakao_email = models.CharField(max_length=100)
     profile_image = models.TextField(default="", null = True)
     nickname = models.CharField(max_length=100)
-    team = models.ForeignKey(Team, null = True)
+    team = models.ForeignKey(Team, null = True, related_name="all_member")
--- team/models.py
+++ team/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from member.models import *
 
 # Create your models here.
 class Team(models.Model):
-    pass
+    master_member = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_station = models.CharField(max_length=100)
+    arrival_station = models.CharField(max_length=100)
+    current_member = models.IntegerField(default=1)
+    maximum_member = models.IntegerField()
