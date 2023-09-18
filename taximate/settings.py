913678b50e2785029d0e93084959a4497707e6e4
ShinSub98 <wjdtjq26@naver.com>
2023-09-18 12:48:53 +0900

    db 연결 작업 중

--- taximate/settings.py
+++ taximate/settings.py
@@ -87,10 +87,21 @@ WSGI_APPLICATION = 'taximate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'taximate',
+        'USER': 'root',
+        'PASSWORD': 'wjdtjq98',
+        'HOST': 'svc.sel5.cloudtype.app',
+        'PORT': '30201'
     }
 }
 
