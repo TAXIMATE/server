b3b31f0be7454c675bda4493b341b21399cc550f
ShinSub98 <wjdtjq26@naver.com>
2023-09-18 12:51:21 +0900

    kakao_logout url 수정

--- member/urls.py
+++ member/urls.py
@@ -6,6 +6,6 @@ app_name = 'member'
 
 urlpatterns = [
     path('login/', KakaoCallBackView.as_view()),
-    path('logout/', views.logout),
+    path('logout/', views.kakao_logout),
     path('gender/', views.check_gender),
 ]
