483c52680ff12526d2a58590742879d8fc711199
ShinSub98 <wjdtjq26@naver.com>
2023-09-19 13:55:17 +0900

    member.urls 오타 수정

--- member/urls.py
+++ member/urls.py
@@ -5,7 +5,7 @@ from .views import *
 app_name = 'member'
 
 urlpatterns = [
-    path('login/', views,kakao_login, name = 'kakao_login'),
+    path('login/', views.kakao_login, name = 'kakao_login'),
     path('logout/', views.kakao_logout, name = 'kakao_logout'),
     path('gender/<str:gender>/', views.check_gender, name = 'check_gender'),
     path('information/', views.user_information, name = 'user_information'),
