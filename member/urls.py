01b5a19652036dd4228f37329ee6b2fac55926f8
ShinSub98 <wjdtjq26@naver.com>
2023-09-22 00:17:58 +0900

    member.urls 수정

--- member/urls.py
+++ member/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from . import views, views_kakao
+from . import views
 from .views import *
 
 app_name = 'member'
 
 urlpatterns = [
+    path('login/', views.kakao_login),
+    path('logout/', views.kakao_logout),
+    path('gender/<str:gender>/', views.check_gender),
+    path('information/', views.user_information),
+    path('kakao/', KakaoView.as_view()),
+    # path('test/', views.test),
+    path('rate/information/<int:team_id>/', views.rate_information),
 ]
