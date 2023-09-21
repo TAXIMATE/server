2629904b3d88b0648cf1bfa7ca34a87d0308426c
ShinSub98 <wjdtjq26@naver.com>
2023-09-22 00:19:04 +0900

    팀 생성 메소드 작성

--- team/views.py
+++ team/views.py
@@ -25,3 +25,34 @@ def waiting_teams(request):
         },
     }
     return Response(res)
+
+
+class Create_team(CreateAPIView):
+    serializer_class = TeamCreateSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+
+    def create(self, request):
+        user = self.request.user
+        if Team.objects.filter(Q(master_member = user)|Q(usual_member = user)).exists():
+            res = {
+                "msg" : "이미 팀에 소속된 사용자",
+                "code" : "t-F005"
+            }
+            return Response(res)
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        team = Team.objects.get(master_member = user)
+        team_id = TeamIDSerializer(team).data["id"]
+        res = {
+            "msg" : "팀 생성 성공",
+            "code" : "t-S002",
+            "data" : {
+                "team_id" : team_id
+            }
+        }
+        return Response(res)
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(master_member = user)
