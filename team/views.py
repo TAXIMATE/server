78ba3b97bf26e7bbc04af6d2c93ed5200ace99f3
ShinSub98 <wjdtjq26@naver.com>
2023-09-22 00:18:43 +0900

    대기 중 팀 수 조회 메소드 작성

--- team/views.py
+++ team/views.py
@@ -10,46 +10,18 @@ from member.serializers import *
 from django.db.models import Q
 import requests
 from rest_framework.decorators import api_view, authentication_classes,permission_classes
-from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+import json
 
-# Create your views here.
+
 @api_view(['GET'])
-def number_of_team(request):
-    number = Team.objects.all().count()
-    data = {
-        "teams" : number
+def waiting_teams(request):
+    num = Team.objects.all().count()
+    res = {
+        "msg" : "전체 팀 수 조회에 성공",
+        "code" : "t-S001",
+        "data" : {
+            "teams" : num
+        },
     }
-    return Response(data)
-
-
-@api_view(['POST'])
-@authentication_classes([JWTAuthentication])
-def create_team(request):
-    serializer = TeamCreateSerializer(data = request.data, master_member = request.user)
-    if serializer.is_valid():
-        serializer.save()
-        Response(status = status.HTTP_200_OK)
-    Response(status = status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['POST'])
-@authentication_classes([JWTAuthentication])
-def participate_team(request, team_id):
-    team = Team.objects.get(pk = team_id)
-    user = CustomUser(request.user)
-    if team.usual_member.filter(pk = user.pk).exists():
-        team.usual_member.remove(user)
-        team.current_member -= 1
-    else:
-        team.usual_member.add(user)
-        team.current_member += 1
-    team.save()
-    return Response(status = status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-@authentication_classes([JWTAuthentication])
-def team_detail(request, team_id):
-    team = Team.objects.get(pk = team_id)
-    serializer = TeamDetailSerializer(team)
-    return Response
+    return Response(res)
