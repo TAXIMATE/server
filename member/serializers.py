c4790fa2eb1edbeb82fb45840c03a110bb9afaeb
ShinSub98 <wjdtjq26@naver.com>
2023-09-22 00:16:36 +0900

    member.serializer 수정

--- member/serializers.py
+++ member/serializers.py
@@ -6,10 +6,22 @@ from .models import *
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['user_id', 'kakao_id', 'profile_image', 'nickname', 'gender', 'temperature', 'team']
+        fields = ['kakao_id', 'profile_image', 'nickname', 'gender', 'temperature']
 
 # 대기방에서 필요한 유저 정보
 class UserSimpleSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['nickname', 'profile_image', 'gender', 'temperature']
+
+
+class UserCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['profile_image', 'nickname']
+
+
+class UserRateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['kakao_id', 'nickname', 'profile_image']
