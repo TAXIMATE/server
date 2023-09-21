1ac71c9528623f18a2a691c8049466556cff1574
ShinSub98 <wjdtjq26@naver.com>
2023-09-22 00:21:19 +0900

    team.urls 수정

--- team/urls.py
+++ team/urls.py
@@ -5,5 +5,14 @@ from .views import *
 app_name = 'team'
 
 urlpatterns = [
-    path('', views.number_of_team),
+    path('all/', views.all_teams),
+    path('', views.waiting_teams),
+    # path('create/', views.create_team),
+    path('create/', Create_team.as_view()),
+    path('delete/<int:team_id>/', views.destroy_team),
+    path('participate/<int:team_id>/', views.participate_team),
+    path('detail/<int:team_id>/', views.team_detail),
+    path('detail/comments/<int:team_id>/', views.get_comments),
+    path('detail/comments/create/<int:team_id>/', views.create_comment),
+    path('search/', views.search_team),
 ]
