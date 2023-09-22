332d1a5dea8d61d122834ccc30f2847514846a17
ShinSub98 <wjdtjq26@naver.com>
2023-09-22 15:03:46 +0900

    CORS 오류 수정

--- taximate/settings.py
+++ taximate/settings.py
@@ -140,8 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
-    "http://127.0.0.1:8000"]
-
+    "http://127.0.0.1:8000",
+    "https://port-0-server-2rrqq2blmoc3kpx.sel5.cloudtype.app"]
 
 
 CORS_ALLOW_CREDENTIALS = True
@@ -156,4 +156,5 @@ pymysql.install_as_MySQLdb()
 
 AUTH_USER_MODEL = 'member.CustomUser'
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", 'https://port-0-server-2rrqq2blmoc3kpx.sel5.cloudtype.app']
+
