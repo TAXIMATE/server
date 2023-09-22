959aad2fdf3a9d4ce8a9991e52f74c20d5884f4a
ShinSub98 <wjdtjq26@naver.com>
2023-09-22 14:44:11 +0900

    테스트 브랜치 생성

--- member/urls.py
+++ member/urls.py
@@ -5,11 +5,11 @@ from .views import *
 app_name = 'member'
 
 urlpatterns = [
-    path('login/', views.kakao_login),
+    path('kakao/', KakaoView.as_view()),
+    path('login/<str:code>/', views.kakao_login),
     path('logout/', views.kakao_logout),
     path('gender/<str:gender>/', views.check_gender),
     path('information/', views.user_information),
-    path('kakao/', KakaoView.as_view()),
     # path('test/', views.test),
     path('rate/information/<int:team_id>/', views.rate_information),
 ]
--- member/views.py
+++ member/views.py
@@ -19,7 +19,8 @@ from rest_framework.authentication import SessionAuthentication, BasicAuthentica
 class KakaoView(View):
     def get(self, request):
         kakao_api = "https://kauth.kakao.com/oauth/authorize?response_type=code"
-        redirect_uri = "http://127.0.0.1:8000/member/login/"
+        # redirect_uri = "http://127.0.0.1:8000/member/login/"
+        redirect_uri = "http://localhost.3000/wait"
         # redirect_uri = "https://port-0-server-2rrqq2blmoc3kpx.sel5.cloudtype.app/member/login/"
         client_id = "d679f25e59dbc97619baf1256489b449"
 
@@ -28,15 +29,15 @@ class KakaoView(View):
 
 # 카카오 로그인
 @api_view(['POST'])
-def kakao_login(request):
+def kakao_login(request, code):
     data = {
         "grant_type" : "authorization_code",
         "client_id" : "d679f25e59dbc97619baf1256489b449",
-        # "redirect_uri" : "http://localhost.3000/wait",
-        "redirect_uri" : "http://127.0.0.1:8000/member/login/",
+        "redirect_uri" : "http://localhost.3000/wait",
+        # "redirect_uri" : "http://127.0.0.1:8000/member/login/",
         # "redirect_uri" : "https://port-0-server-2rrqq2blmoc3kpx.sel5.cloudtype.app/member/login/",
-        "code" : request.GET["code"]
-        # "code" : json.loads(request.body).get("code")
+        # "code" : request.GET["code"]
+        "code" : code
     }
 
     kakao_token_api = "https://kauth.kakao.com/oauth/token"
