41c211e001c7f9230efebc9cb5a0ba5b458b0f6c
ShinSub98 <wjdtjq26@naver.com>
2023-09-22 14:59:07 +0900

    테스트 브랜치 업데이트

--- member/urls.py
+++ member/urls.py
@@ -5,11 +5,15 @@ from .views import *
 app_name = 'member'
 
 urlpatterns = [
-    path('kakao/', KakaoView.as_view()),
     path('login/<str:code>/', views.kakao_login),
     path('logout/', views.kakao_logout),
     path('gender/<str:gender>/', views.check_gender),
     path('information/', views.user_information),
     # path('test/', views.test),
     path('rate/information/<int:team_id>/', views.rate_information),
+    
+    ## 개발자용 url
+    path('kakao/', KakaoView.as_view()),
+    path('login/', views.kakao_login_dev),
+    path('all/', views.all_member),
 ]
