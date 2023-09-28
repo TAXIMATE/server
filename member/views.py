8d556020060958071dbf663f220e22c95a5566de
ShinSub98 <wjdtjq26@naver.com>
2023-09-28 13:11:12 +0900

    유저 환경 변경

--- member/views.py
+++ member/views.py
@@ -43,46 +43,73 @@ def kakao_login(request, code):
 
     user = CustomUser.objects.filter(kakao_id=kakao_id).first()
     if user is not None:
-        # 프로필 사진이 바뀌었을 경우 적용
         if user.profile_image != profile_image:
             user.profile_image = profile_image
-            user.save()
-        # 닉네임이 바뀌었을 경우 적용
         if user.nickname != nickname:
             user.nickname = nickname
-            user.save()
-        if user.gender != None:
-            auth.login(request, user=user)
-            # serializer = UserSimpleSerializer(user)
-            res_data = {
-                "msg" : "기존 사용자 로그인 성공",
-                "code" : "m-S002",
-                "data" : {
-                    "gender_needed" : True
-                }
-            }
-            return Response(res_data)
-        else:
-            auth.login(request, user = user)
-            res_data = {
-                "msg" : "신규 가입자 로그인 성공",
-                "code" : "m-S001",
-                "data" : {
-                    "gender_needed" : False
-                }
-            }
-            return Response(res_data)
+        user.save()
+        serializer = UserSerializer(user)
+        auth.login(request, user = user)
+        res = {
+            "msg" : "기존 사용자 로그인 성공",
+            "code" : "m-S002",
+            "data" : serializer.data
+        }
+        return Response(res)
+
     new_user = CustomUser(kakao_id = kakao_id, profile_image = profile_image, nickname = nickname)
     new_user.save()
+    serializer = UserSerializer(new_user)
     auth.login(request, new_user)
-    res_data = {
+    res = {
         "msg" : "신규 가입자, 로그인 성공",
         "code" : "m-S001",
-        "data" : {
-            "gender_needed" : False
-        }
+        "data" : serializer.data
     }
-    return Response(res_data)
+    return Response(res)   
+    
+    
+    # if user is not None:
+    #     # 프로필 사진이 바뀌었을 경우 적용
+    #     if user.profile_image != profile_image:
+    #         user.profile_image = profile_image
+    #         user.save()
+    #     # 닉네임이 바뀌었을 경우 적용
+    #     if user.nickname != nickname:
+    #         user.nickname = nickname
+    #         user.save()
+    #     if user.gender != None:
+    #         auth.login(request, user=user)
+    #         # serializer = UserSimpleSerializer(user)
+    #         res_data = {
+    #             "msg" : "기존 사용자 로그인 성공",
+    #             "code" : "m-S002",
+    #             "data" : {
+    #                 "gender_needed" : True
+    #             }
+    #         }
+    #         return Response(res_data)
+    #     else:
+    #         auth.login(request, user = user)
+    #         res_data = {
+    #             "msg" : "신규 가입자 로그인 성공",
+    #             "code" : "m-S001",
+    #             "data" : {
+    #                 "gender_needed" : False
+    #             }
+    #         }
+    #         return Response(res_data)
+    # new_user = CustomUser(kakao_id = kakao_id, profile_image = profile_image, nickname = nickname)
+    # new_user.save()
+    # auth.login(request, new_user)
+    # res_data = {
+    #     "msg" : "신규 가입자, 로그인 성공",
+    #     "code" : "m-S001",
+    #     "data" : {
+    #         "gender_needed" : False
+    #     }
+    # }
+    # return Response(res_data)
 
 
 # 카카오 로그아웃
