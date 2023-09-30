bbff1a9b7e7e6502a53d8ba1b0e8130f5d45f767
ShinSub98 <wjdtjq26@naver.com>
2023-09-30 14:40:24 +0900

    team views 정리 및 delete team -> cancel team

--- team/urls.py
+++ team/urls.py
@@ -1,22 +1,20 @@
 from django.urls import path
-from . import views, views_dev
-from .views import *
-from .views_dev import *
+from team.views import *
 
 app_name = 'team'
 
 urlpatterns = [
-    path('', views.waiting_teams),
+    path('', waiting_teams),
     # path('create/', views.create_team),
     path('create/', Create_team.as_view()),
-    path('delete/<int:team_id>/', views.destroy_team),
-    path('participate/<int:team_id>/', views.participate_team),
-    path('detail/<int:team_id>/', views.team_detail),
-    path('detail/comments/<int:team_id>/', views.get_comments),
-    path('detail/comments/create/<int:team_id>/', views.create_comment),
-    path('search/', views.search_team),
-    path('start/<int:team_id>/', views.team_start),
-    path('own/', views.user_in_team),
+    path('cancel/<int:team_id>/', cancel_team),
+    path('participate/<int:team_id>/', participate_team),
+    path('detail/<int:team_id>/', team_detail),
+    path('detail/comments/<int:team_id>/', get_comments),
+    path('detail/comments/create/<int:team_id>/', create_comment),
+    path('search/', search_team),
+    path('start/<int:team_id>/', team_start),
+    path('own/', user_in_team),
 
     ## 개발자용 api
     path('all/', views_dev.all_teams),
---
+++ team/views/views_comment.py
@@ -0,0 +1,45 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from ..serializers import *
+from ..models import *
+from member.models import *
+from member.serializers import *
+from django.db.models import Q
+from rest_framework.decorators import api_view, authentication_classes,permission_classes
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+import json
+from datetime import datetime
+
+# 댓글 작성
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication, BasicAuthentication])
+def create_comment(request, team_id):
+    team = Team.objects.get(pk = team_id)
+    user = request.user
+    serializer = CommentCreateSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save(team = team, member = user)
+        res = {
+            "msg" : "댓글 작성 성공",
+            "code" : "t-S007"
+        }
+        return Response(res)
+    res = {
+        "msg" : "댓글 작성 실패",
+        "code" : "t-F004"
+    }
+    return Response(res)
+
+
+# 댓글 목록 가져오기
+@api_view(['GET'])
+def get_comments(request, team_id):
+    team = Team.objects.get(pk = team_id)
+    serializer = TeamCommentsSerializer(team)
+    res = {
+        "msg" : "전체 댓글 불러오기 성공",
+        "code" : "t-S006",
+        "data" : serializer.data
+    }
+    return Response(res)
