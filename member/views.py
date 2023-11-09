a64a839138ea85feb77a298d477ce229183893f8
ShinSub98 <wjdtjq26@naver.com>
2023-11-09 12:13:22 +0900

    test: get uri

--- member/views.py
+++ member/views.py
@@ -19,111 +19,84 @@ from django.contrib.auth import login
 
 # Create your views here.
 # 카카오 로그인
+# @api_view(['GET'])
+# def kakao_login(request, code):
+#     data = {
+#         "grant_type" : "authorization_code",
+#         "client_id" : "d679f25e59dbc97619baf1256489b449",
+#         "redirect_uri" : "https://taximate-nine.vercel.app/wait",
+#         # "redirect_uri" : "http://127.0.0.1:8000/member/login/",
+#         # "redirect_uri" : "https://port-0-server-2rrqq2blmoc3kpx.sel5.cloudtype.app/member/login/",
+#         # "code" : request.GET["code"]
+#         "code" : code
+#     }
+
+#     kakao_token_api = "https://kauth.kakao.com/oauth/token"
+#     # access_token = requests.post(kakao_token_api, data = data).json()["access_token"]
+#     response = requests.post(kakao_token_api, data=data)
+#     res_data = response.json()
+#     kakao_access_token = res_data.get("access_token")
+
+#     kakao_user_api = "https://kapi.kakao.com/v2/user/me"
+#     header = {"Authorization":f"Bearer ${kakao_access_token}"}
+#     user_information = requests.get(kakao_user_api, headers = header).json()
+    
+#     kakao_id = user_information["id"]
+#     profile_image = user_information["kakao_account"]["profile"]["thumbnail_image_url"]
+#     nickname = user_information["properties"]["nickname"]
+
+#     user = CustomUser.objects.filter(kakao_id=kakao_id).first()
+#     if user is not None:
+#         if user.profile_image != profile_image:
+#             user.profile_image = profile_image
+#         if user.nickname != nickname:
+#             user.nickname = nickname
+#         user.save()
+#         serializer = UserSerializer(user)
+#         login(request, user)
+#         token = TokenObtainPairSerializer.get_token(user)
+#         access_token = str(token.access_token)
+#         res = {
+#             "msg" : "기존 사용자 로그인 성공",
+#             "code" : "m-S002",
+#             "data" : {
+#                 "user_data" : serializer.data,
+#                 "access_token" : access_token
+#             }
+#         }
+#         return Response(res)
+
+#     new_user = CustomUser(kakao_id = kakao_id, profile_image = profile_image, nickname = nickname)
+#     new_user.save()
+#     serializer = UserSerializer(new_user)
+#     login(request, new_user)
+#     token = TokenObtainPairSerializer.get_token(new_user)
+#     access_token = str(token.access_token)
+#     res = {
+#         "msg" : "신규 가입자, 로그인 성공",
+#         "code" : "m-S001",
+#         "data" : {
+#             "user_data" : serializer.data,
+#             "access_token" : access_token
+#         }
+#     }
+#     return Response(res)   
+
+
 @api_view(['GET'])
 def kakao_login(request, code):
-    data = {
-        "grant_type" : "authorization_code",
-        "client_id" : "d679f25e59dbc97619baf1256489b449",
-        "redirect_uri" : "https://taximate-nine.vercel.app/wait",
-        # "redirect_uri" : "http://127.0.0.1:8000/member/login/",
-        # "redirect_uri" : "https://port-0-server-2rrqq2blmoc3kpx.sel5.cloudtype.app/member/login/",
-        # "code" : request.GET["code"]
-        "code" : code
-    }
-
-    kakao_token_api = "https://kauth.kakao.com/oauth/token"
-    # access_token = requests.post(kakao_token_api, data = data).json()["access_token"]
-    response = requests.post(kakao_token_api, data=data)
-    res_data = response.json()
-    kakao_access_token = res_data.get("access_token")
-
-    kakao_user_api = "https://kapi.kakao.com/v2/user/me"
-    header = {"Authorization":f"Bearer ${kakao_access_token}"}
-    user_information = requests.get(kakao_user_api, headers = header).json()
+    path = request.path
+    full_path = request.get_full_path()
+    absolute_uri = request.build_absolute_uri()
     
-    kakao_id = user_information["id"]
-    profile_image = user_information["kakao_account"]["profile"]["thumbnail_image_url"]
-    nickname = user_information["properties"]["nickname"]
-
-    user = CustomUser.objects.filter(kakao_id=kakao_id).first()
-    if user is not None:
-        if user.profile_image != profile_image:
-            user.profile_image = profile_image
-        if user.nickname != nickname:
-            user.nickname = nickname
-        user.save()
-        serializer = UserSerializer(user)
-        login(request, user)
-        token = TokenObtainPairSerializer.get_token(user)
-        access_token = str(token.access_token)
-        res = {
-            "msg" : "기존 사용자 로그인 성공",
-            "code" : "m-S002",
-            "data" : {
-                "user_data" : serializer.data,
-                "access_token" : access_token
-            }
-        }
-        return Response(res)
-
-    new_user = CustomUser(kakao_id = kakao_id, profile_image = profile_image, nickname = nickname)
-    new_user.save()
-    serializer = UserSerializer(new_user)
-    login(request, new_user)
-    token = TokenObtainPairSerializer.get_token(new_user)
-    access_token = str(token.access_token)
     res = {
-        "msg" : "신규 가입자, 로그인 성공",
-        "code" : "m-S001",
-        "data" : {
-            "user_data" : serializer.data,
-            "access_token" : access_token
-        }
+        "path" : path,
+        "full_path" : full_path,
+        "absolute_uri" : absolute_uri
     }
-    return Response(res)   
-    
-    
-    # if user is not None:
-    #     # 프로필 사진이 바뀌었을 경우 적용
-    #     if user.profile_image != profile_image:
-    #         user.profile_image = profile_image
-    #         user.save()
-    #     # 닉네임이 바뀌었을 경우 적용
-    #     if user.nickname != nickname:
-    #         user.nickname = nickname
-    #         user.save()
-    #     if user.gender != None:
-    #         auth.login(request, user=user)
-    #         # serializer = UserSimpleSerializer(user)
-    #         res_data = {
-    #             "msg" : "기존 사용자 로그인 성공",
-    #             "code" : "m-S002",
-    #             "data" : {
-    #                 "gender_needed" : True
-    #             }
-    #         }
-    #         return Response(res_data)
-    #     else:
-    #         auth.login(request, user = user)
-    #         res_data = {
-    #             "msg" : "신규 가입자 로그인 성공",
-    #             "code" : "m-S001",
-    #             "data" : {
-    #                 "gender_needed" : False
-    #             }
-    #         }
-    #         return Response(res_data)
-    # new_user = CustomUser(kakao_id = kakao_id, profile_image = profile_image, nickname = nickname)
-    # new_user.save()
-    # auth.login(request, new_user)
-    # res_data = {
-    #     "msg" : "신규 가입자, 로그인 성공",
-    #     "code" : "m-S001",
-    #     "data" : {
-    #         "gender_needed" : False
-    #     }
-    # }
-    # return Response(res_data)
+
+    return Response(res)
+
 
 
 # 성별 선택
