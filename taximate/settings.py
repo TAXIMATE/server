f879dfb917f268e6fbbf7d6bdef755111fc63cbf
ShinSub98 <wjdtjq26@naver.com>
2024-01-09 14:09:50 +0900

    chore: use local database

--- taximate/settings.py
+++ taximate/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'taximate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'taximate',
-        'USER': 'root',
-        'PASSWORD': 'wjdtjq98',
-        'HOST': 'svc.sel5.cloudtype.app',
-        'PORT': '30201'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'taximate',
+#         'USER': 'root',
+#         'PASSWORD': 'wjdtjq98',
+#         'HOST': 'svc.sel5.cloudtype.app',
+#         'PORT': '30201'
+#     }
+# }
 
 
 # Password validation
